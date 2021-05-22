db81471e6d65e09b92a0a9be4da7b24525dabfc4
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-05-22 12:21:07 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,11 @@
+#if-else
+#program to read age and check if the student is eligable for vote ot not
+name=input('enter name: ')
+age=int(input('enter age: '))
+if age>=18 :
+    print('Hello',name)
+    print('eligible for vote')
+else :
+    print('sorry!',name)
+    print('not eligible for vote')
+    
