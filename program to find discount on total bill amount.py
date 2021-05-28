6b0cfcf112b3ffe4581df80a06394c51fd09fa89
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-05-28 11:23:17 +0530

    Update and rename task.py to program to find discount on total bill amount.py

--- program to find discount on total bill amount.py
+++ program to find discount on total bill amount.py
@@ -1,23 +1,23 @@
 a=int(input('total bill amount: '))
 if a in range(1000,2000) :
-    dis1=a*0.1
-    print('discount :%.2f'%dis1)
-    paid=a-dis1
+    dis=a*0.1
+    print('discount :%.2f'%dis)
+    paid=a-dis
     print('paid amount is:%d'%paid)
 elif a in range(2000,3000) :
-    dis2=a*0.2
-    print('discount :%.2f'%dis2)
-    paid=a-dis2
+    dis=a*0.2
+    print('discount :%.2f'%dis)
+    paid=a-dis
     print('paid amount is:%d'%paid)
 elif a in range(3000,5000) :
-    dis3=a*0.3
-    print('discount :%.2f'%dis3)
-    paid=a-dis3
+    dis=a*0.3
+    print('discount :%.2f'%dis)
+    paid=a-dis
     print('paid amount is:%d'%paid)
 elif a > 5000 :
-    dis4=a*0.4
-    print('discount :%.2f'%dis4)
-    paid=a-dis4
+    dis=a*0.4
+    print('discount :%.2f'%dis)
+    paid=a-dis
     print('paid amount is:%d'%paid)
 else :
     print('no discount')
