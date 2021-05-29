7a34e40922757181b7ebfdefa47d9b84b24851a8
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-05-29 13:51:07 +0530

    Update factors of given number.py

--- factors of given number.py
+++ factors of given number.py
@@ -1,12 +1,13 @@
 #finding factors of a number
 n=int(input('enter number: '))
 i=1
+temp=n
 print('factors of %d are : '%n)
-while i<=n:
+while i<=n//2:
     if n%i==0:
         print(i,end="   ")
     i+=1
-
+print(temp)
 
 '''
 expected output
