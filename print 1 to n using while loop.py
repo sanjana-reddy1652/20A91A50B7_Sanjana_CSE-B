d68d6657db302de076f7272b9748272f8d439cb2
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-05-29 09:29:31 +0530

    Create print 1 to n using while loop.py

---
+++ print 1 to n using while loop.py
@@ -0,0 +1,16 @@
+#printing 1 to n 
+n=int(input('enter range: '))
+i=1
+while i in range (1, n+1):
+  print(i, end=" ")
+  i+=1
+
+  
+'''
+expected output
+enter range: 10
+1 2 3 4 5 6 7 8 9 10
+'''
+
+  
+
