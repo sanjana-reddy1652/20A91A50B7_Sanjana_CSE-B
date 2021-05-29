c0c97836ae50243e2a7fbff5cd7c712eb27d9684
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-05-29 14:33:58 +0530

    Create power of a number.py

---
+++ power of a number.py
@@ -0,0 +1,13 @@
+a=int(input('enter base: '))
+b=int(input('enter power: '))
+c=a**b
+r=c%10
+print('last digit of %d is %d '%(c,r))
+'''
+expected output 
+
+enter base: 2
+enter power: 4
+last digit of 16 is 6 
+
+'''
