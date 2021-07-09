dbc7b8308e7287f91f560763bf5acc305f2eadfb
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-07-09 19:08:20 +0530

    Create Methods Part_1.py

---
+++ Methods Part_1.py
@@ -0,0 +1,29 @@
+# abs() : function returns the absolute value of the specified number
+x = abs(3+5j)
+print(x)
+# output : 5.830951894845301
+
+
+# all() : function returns True if all items in an iterable are true, otherwise it returns False
+mylist = [True, True, True]
+x = all(mylist)
+print(x)
+# output : True
+
+
+# any() :	Returns True if any item in an iterable object is true
+mylist = [False, True, False]
+x = any(mylist)
+print(x)
+#output : True
+
+
+# ascii() :	Returns a readable version of an object. Replaces none-ascii characters with escape character 
+x = ascii("My name is Sånjana")
+print(x)
+#output : 'My name is S\xe5njana'
+
+
+
+
+
